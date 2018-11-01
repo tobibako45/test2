4b3975b466caf30a774979733b8eafb03392c21d
Yasuhiro Shimoji <>
2018-11-01 22:06:25 +0900

    ss

--- try1.rb
+++ try1.rb
@@ -1,2 +1,14 @@
-a = 2 + 2
-puts a
+range = 1..100
+
+
+range.each{|num|
+  if num % 15 ==0 then
+    puts "FizzBuzz"
+  elsif num % 3 == 0 then
+    puts "Fizz"
+  elsif num % 5 == 0 then
+    puts "Buzz"
+  else
+    puts num
+  end
+}
